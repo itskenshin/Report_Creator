801a4c916e075360d30b193d63bc81c01b9f7617
Kenshin <zomaliense01@gmail.com>
2022-07-26 16:38:25 -0400

    Add project files.

---
+++ forms/Form_orden_compra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Facturacion
+{
+    public partial class Form_orden_compra : Form
+    {
+        public Form_orden_compra()
+        {
+            InitializeComponent();
+        }
+    }
+}
