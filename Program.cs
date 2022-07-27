ecbfbb2533ef70ecbe771e434c2047bc67322015
Kenshin <zomaliense01@gmail.com>
2022-07-27 02:25:42 -0400

    formulario coti modificado

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Facturacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new Form_cotiza());
         }
     }
 }
